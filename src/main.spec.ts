11c68c28cd305cf99b2807934b8852a0eed2decc
Dominic <dominicshajaa@icloud.com>
2022-05-27 14:43:40 -0700

    fixed a few issues

---
+++ src/main.spec.ts
@@ -0,0 +1,7 @@
+import { main } from "./main";
+
+describe("Basic main test", () => {
+  it("should be defined", () => {
+    expect(main).toBeDefined();
+  });
+});
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,13 @@
-console.debug("Fuck");
+import chalk from "chalk";
+
+export function main() {
+  console.log(chalk.blue("Hello boiler 😉"));
+  console.log(
+    chalk.magenta(
+      "This is a basic TS node boilderplate\nI'll be using this in a lot of my projects!"
+    )
+  );
+
+  return 0;
+}
+main();
